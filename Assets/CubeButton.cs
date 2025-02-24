691e54159d872b7fe23d0586f910f4bd16d69d86
Anna Cubine <acubine10@gmail.com>
2025-02-24 13:42:09 -0800

    Added color change to room and fixed compatability issues and texture issures.

--- Assets/CubeButton.cs
+++ Assets/CubeButton.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Drawing;
 using UnityEngine;
 
 public class CubeButton : MonoBehaviour
@@ -7,13 +8,16 @@ public class CubeButton : MonoBehaviour
     public Direction direction;
 
     //Getting original color of button and doors so the color change is only for a second
-    public Color originalColor;
-    public Color doorColor;
+    public UnityEngine.Color originalColor;
+    public UnityEngine.Color doorColor;
 
     //Doors to open when button is pressed
     public GameObject doorLeft;
     public GameObject doorRight;
     public GameObject doorUp;
+    public GameObject[] walls;
+
+    //private UnityEngine.Color newColor = new UnityEngine.Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
 
     void Start()
     {
@@ -40,12 +44,17 @@ public class CubeButton : MonoBehaviour
             StartCoroutine(OpenDoor(doorUp));
             Debug.Log("Go up");
         }
+        UnityEngine.Color newColor = UnityEngine.Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        for (int i = 0; i < walls.Length; i++)
+        {
+            walls[i].GetComponent<Renderer>().material.color = newColor;
+        }
     }
 
     private IEnumerator ChangeColor()
     {
         //Giving player indication that button has been pressed
-        GetComponent<Renderer>().material.color = Color.white;
+        GetComponent<Renderer>().material.color = UnityEngine.Color.white;
         //Wait .2 seconds...
         yield return new WaitForSeconds(0.2f);
         //Return color to its original color
@@ -54,7 +63,7 @@ public class CubeButton : MonoBehaviour
 
     private IEnumerator OpenDoor(GameObject door)
     {
-        door.GetComponent<Renderer>().material.color = Color.black;
+        door.GetComponent<Renderer>().material.color = UnityEngine.Color.black;
         yield return new WaitForSeconds(0.4f);
         door.GetComponent <Renderer>().material.color = doorColor;
     }
